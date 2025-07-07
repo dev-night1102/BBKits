5c719c12bacc4eae7197446e3dc582051db78ba2
hunterdev90 <Hunterdev90@outlook.com>
2025-07-07 15:49:17 -0300

    added: public folder

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.jsx', 'resources/css/app.css'],
-            refresh: true,
-        }),
-        react(),
-    ],
-    css: {
-        postcss: {
-            plugins: [
-                tailwindcss(),
-                autoprefixer(),
-            ],
-        },
-    },
-});
+  plugins: [
+    laravel({
+      input: ['resources/js/app.jsx', 'resources/css/app.css'],
+      refresh: true,
+    }),
+    react(),
+  ],
+})
