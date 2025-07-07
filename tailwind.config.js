1ff7a06991ec7c95f47569f1960d0767724ae530
hunterdev90 <Hunterdev90@outlook.com>
2025-07-07 16:56:07 -0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
+// tailwind.config.js
 export default {
   content: [
     './resources/**/*.blade.php',
     './resources/**/*.js',
     './resources/**/*.jsx',
-    './resources/**/*.vue',
   ],
   theme: {
     extend: {},
