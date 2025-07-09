dd408a36c9236f4f5896c471dcfeb756b7cdc3d7
hunterdev90 <Hunterdev90@outlook.com>
2025-07-09 17:02:26 -0300

    stress

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.jsx', 'resources/css/app.css'],
+            input: ["resources/js/app.jsx", "resources/css/app.css"],
             refresh: true,
         }),
         react(),
     ],
+    server: {
+        hmr: false,
+    },
     css: {
         postcss: {
-            plugins: [
-                tailwindcss(),
-                autoprefixer(),
-            ],
+            plugins: [tailwindcss(), autoprefixer()],
         },
     },
 });
