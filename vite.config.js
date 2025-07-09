64b41f760c37a634affaa5c03bde6a739a3da8fd
hunterdev90 <Hunterdev90@outlook.com>
2025-07-09 15:01:44 -0300

    rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.jsx', 'resources/css/app.css'],
+            input: ["resources/js/app.jsx", "resources/css/app.css"],
             refresh: true,
         }),
         react(),
     ],
+    build: {
+        outDir: "public/build",
+        manifest: true,
+    },
     css: {
         postcss: {
-            plugins: [
-                tailwindcss(),
-                autoprefixer(),
-            ],
+            plugins: [tailwindcss(), autoprefixer()],
         },
     },
 });
